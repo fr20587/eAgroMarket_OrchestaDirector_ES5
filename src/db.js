787103910d6f27655b20c5adb00f851609c26871
Frank Rodriguez Lopez <fr20587@outlook.com>
2021-02-02 10:19:27 -0500

    Trabajando en la conexion con mongo on line

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var _mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-var _mongoose2 = _interopRequireDefault(_mongoose);
+//var _mongoose2 = _interopRequireDefault(mongoose);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /* _mongoose2.default.connect('mongodb://localhost/AgroMarketDB', {
     useNewUrlParser: true,
